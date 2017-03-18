e15be76c656b9129f0b9aed18296d3cf3b7ae3db
nakarin0528 <n1510121@edu.cc.uec.ac.jp>
2017-03-18 17:49:38 +0900

    端末間データやりとり一応できた

---
+++ TabbedFC/RoomViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RoomViewController.swift
+//  TabbedFC
+//
+//  Created by I,N on 2017/03/18.
+//  Copyright © 2017年 feelingCouplePBL. All rights reserved.
+//
+
+import UIKit
+
+class RoomViewController: UIViewController {
+
+    
+    var ble = BLEP.sharedBleP
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
