e00e7377b9b95ea45cf2b9ae0855eb8b4aaf20aa
Syunpei Nagata <n1510124@edu.cc.uec.ac.jp>
2017-03-20 12:28:55 +0900

    データ処理（仮）

---
+++ GetPersonalDataDeligate.swift
@@ -0,0 +1,13 @@
+//
+//  GetPersonalDataDeligate.swift
+//  TabbedFC
+//
+//  Created by 永田駿平 on 2017/03/20.
+//  Copyright © 2017年 feelingCouplePBL. All rights reserved.
+//
+
+import Foundation
+
+protocol GetPersonalDataDeligate {
+    func setPersonalData(value: [Any])
+}
---
+++ TabbedFC/OrganizingData.swift
@@ -0,0 +1,29 @@
+//
+//  OrganizingData.swift
+//  TabbedFC
+//
+//  Created by 永田駿平 on 2017/03/20.
+//  Copyright © 2017年 feelingCouplePBL. All rights reserved.
+//
+
+import Foundation
+
+class OrganizingData: GetPersonalDataDeligate {
+    var participants = [[Any]]()
+    var males = [[Any]]()
+    var females = [[Any]]()
+    
+    func setPersonalData(value: [Any]) {
+        participants.append(value)
+    }
+
+    func separateByGender() {
+        for i in 0...participants.count {
+            if (participants[i][1] as! Int) == 0 {
+                males.append(participants[i])
+            } else {
+                females.append(participants[i])
+            }
+        }
+    }
+}
